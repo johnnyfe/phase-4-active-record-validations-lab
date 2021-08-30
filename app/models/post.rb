c8fb4a9044a371e956e9e4798232c356924c757b
johnny <johnny1194@hotmail.com>
2021-08-29 22:11:19 -0400

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ["Fiction","Non-Fiction"]}
+    validate :clickbait?
+
+    Clickbaits = [/Believe/i, /Guess/i, /Top/i, /Popular/i]
+
+    def clickbait?
+        if Clickbaits.none? {|path| path.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
